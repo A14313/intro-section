bf95e166f188a25e3c71e98a5878582851445533
Carlo Autor <carloautor@gmail.com>
2022-12-06 19:21:47 +0800

    Updated Readme

---
+++ dist/index.cab5d73a.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".hamburger__icon"),t=document.querySelectorAll(".with-arrow");e.addEventListener("click",(()=>{e.classList.toggle("active")})),t.forEach((e=>{e.addEventListener("click",(()=>{e.classList.toggle("active")}))}));
+//# sourceMappingURL=index.cab5d73a.js.map
