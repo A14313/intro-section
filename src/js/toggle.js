dac5ddcd492596aa77fc8ddf76db2e58157496d4
Carlo Autor <carloautor@gmail.com>
2022-12-06 19:18:52 +0800

    Done with mobile layout
    Added support for tablet devices (800px)

--- src/js/toggle.js
+++ src/js/toggle.js
@@ -1,5 +1,12 @@
-const navToggle = document.querySelector(".hamburger-container");
+const navToggle = document.querySelector(".hamburger__icon");
+const navToggleLists = document.querySelectorAll(".with-arrow");
 
 navToggle.addEventListener("click", () => {
 	navToggle.classList.toggle("active");
+});
+
+navToggleLists.forEach((navToggleList) => {
+	navToggleList.addEventListener("click", () => {
+		navToggleList.classList.toggle("active");
+	});
 });
